d5303cb0627f568c74a814c03682dec56c3d88f2
aldazar <alexandreblanchet44@gmail.com>
2023-01-26 20:55:40 +0000

    fix: add example for mixins

--- design_patterns/mixin/model_mixins.py
+++ design_patterns/mixin/model_mixins.py
@@ -1,11 +1,13 @@
 
-# Second hand
+# Bespoke furniture store
+
 
 class DimensionMixin:
 
-    def __init__(self, width, height, weight):
+    def __init__(self, width, height, length, weight):
         self.width = width
         self.height = height
+        self.length = length
         self.weight = weight
 
 
@@ -19,7 +21,7 @@ class Table(Product, DimensionMixin):
     pass
 
 
-class Chair(Product, DimensionMixin):
+class Leg(Product, DimensionMixin):
     pass
 
 
@@ -27,3 +29,17 @@ class CardboardBox(DimensionMixin):
     pass
 
 
+# For the given table and table legs,
+# select a cardboard fitting perfectly those dimensions
+# 4 legs and one table
+
+# cardboard_1 = CardboardBox(20, 30, 50, 0.200)
+# cardboard_2 = CardboardBox(40, 40, 40, 0.200)
+# cardboard_3 = CardboardBox(60, 30, 60, 0.200)
+#
+# table = Table(20, 20, 5, 1)
+#
+# leg_1 = Leg(2, 2, 5, 0.100)
+# leg_2 = Leg(2, 2, 5, 0.100)
+# leg_3 = Leg(2, 2, 5, 0.100)
+# leg_4 = Leg(2, 2, 5, 0.100)
